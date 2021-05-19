36e3378ce8fa4b8910f51290b99b5171793f47a1
Vo Tat Dat <tatdat13@gmail.com>
2021-05-19 19:38:52 +0700

    Test with coverage

--- 3_Blog/config/settings.py
+++ 3_Blog/config/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd-party apps
+    'rest_framework',
+
     # Local
     'posts',
 ]
@@ -136,3 +139,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
--- 3_Blog/posts/tests.py
+++ 3_Blog/posts/tests.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a user
+        test_user1 = User.objects.create_user(
+            username='test_user1', password='abc123')
+        test_user1.save()
+        # Create a blog post
+        test_post = Post.objects.create(
+            author=test_user1, title='Blog title', body='Body content...')
+        test_post.save()
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(author, 'test_user1')
+        self.assertEqual(title, 'Blog title')
+        self.assertEqual(body, 'Body content...')
