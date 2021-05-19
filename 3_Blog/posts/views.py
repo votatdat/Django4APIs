7c56c177ed790a3dd00ba7b4b364a4749e4f0190
Vo Tat Dat <tatdat13@gmail.com>
2021-05-19 23:03:37 +0700

    Added permission

--- 3_Blog/posts/views.py
+++ 3_Blog/posts/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
@@ -10,5 +11,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
